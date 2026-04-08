88061bb1d6b2b61150b41514d722d06c2cf6b71e
raultejada24 <raulintm@gmail.com>
2026-04-08 19:07:58 +0200

    Add method to retrieve user's preferred notification type in Account class

--- src/main/java/es/codeurjc/model/Account.java
+++ src/main/java/es/codeurjc/model/Account.java
@@ -97,6 +97,15 @@ public class Account {
     public void setUser(User user) {
         this.user = user;
     }
+
+    /**
+     * Issue 14: Delegated method to get user's preferred notification type
+     * Follows Law of Demeter - Account handles accessing User's notification preference
+     * @return User's preferred notification type
+     */
+    public User.NotificationType getPreferredNotificationType() {
+        return user.getNotificationType();
+    }
     
     public List<Transaction> getTransactions() {
         return transactions;
